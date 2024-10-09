0f8782e9e07d90b1d867b669b5e8b942734595ce
Julien Grach <julien.grach@justice.fr>
2024-10-09 15:05:11 +0200

    fix: remove auth for tests

--- src/api/infrastructure/controllers/decision/decision.controller.ts
+++ src/api/infrastructure/controllers/decision/decision.controller.ts
@@ -33,7 +33,6 @@ import { InfrastructureExpection } from '../../../../shared/infrastructure/excep
 import { UnexpectedException } from '../../../../shared/infrastructure/exceptions/unexpected.exception'
 import { SaveDecisionUsecase } from '../../../usecase/saveDecision.usecase'
 import { DecisionS3Repository } from '../../../../shared/infrastructure/repositories/decisionS3.repository'
-import { ClientNotAuthorizedException } from '../../../../shared/infrastructure/exceptions/clientNotAuthorized.exception'
 
 export interface DecisionResponse {
   jsonFileName: string | void
@@ -81,7 +80,6 @@ export class DecisionController {
     metadonneeDto: MetadonneeDto,
     @Req() request: Request
   ): Promise<DecisionResponse> {
-    checkBasicAuth(request)
 
     if (!fichierDecisionIntegre || !isPdfFile(fichierDecisionIntegre.mimetype)) {
       throw new BadFileFormatException('fichierDecisionIntegre', 'PDF')
@@ -139,25 +137,4 @@ export class DecisionController {
 
 export function isPdfFile(mimeType: string): boolean {
   return mimeType === 'application/pdf'
-}
-
-export function checkBasicAuth(req: Request) {
-  const users = {
-    [process.env.DOC_LOGIN]: process.env.DOC_PASSWORD
-  }
-  const authHeader = req.headers.authorization
-  if (!authHeader) {
-    throw new ClientNotAuthorizedException('Authorization header missing')
-  }
-  const [scheme, credentials] = authHeader.split(' ')
-
-  if (scheme !== 'Basic' || !credentials) {
-    throw new ClientNotAuthorizedException('Invalid authorization scheme')
-  }
-
-  const [username, password] = Buffer.from(credentials, 'base64').toString().split(':')
-
-  if (users[username] !== password) {
-    throw new ClientNotAuthorizedException('Invalid credentials')
-  }
 }

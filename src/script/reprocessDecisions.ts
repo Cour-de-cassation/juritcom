2699cf79bfda4c17e64e0e54ac4127ebaa9ba43a
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2025-03-07 09:07:50 +0100

    Create reprocessDecisions.ts

---
+++ src/script/reprocessDecisions.ts
@@ -0,0 +1,31 @@
+import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3'
+
+async function main(id: string) {
+  const pdf: Buffer = await getPDFByFilename(`${id}.pdf`)
+}
+
+async function getPDFByFilename(filename: string): Promise<Buffer> {
+  const s3Client = new S3Client({
+    endpoint: process.env.S3_URL,
+    forcePathStyle: true,
+    region: process.env.S3_REGION,
+    credentials: {
+      accessKeyId: process.env.S3_ACCESS_KEY,
+      secretAccessKey: process.env.S3_SECRET_KEY
+    }
+  })
+
+  const reqParams = {
+    Bucket: process.env.S3_BUCKET_NAME_PDF,
+    Key: filename
+  }
+
+  try {
+    const fileFromS3 = await s3Client.send(new GetObjectCommand(reqParams))
+    return Buffer.from(await fileFromS3.Body?.transformToByteArray())
+  } catch (error) {
+    console.log({ operationName: 'getPDFByFilename', msg: error.message, data: error })
+  }
+}
+
+main(process.argv[2])

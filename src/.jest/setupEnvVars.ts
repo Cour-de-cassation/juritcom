bc2f77b5864841fa132c8ad759ccb1ba551a0ded
Julien Grach <julien.grach@justice.fr>
2025-10-01 15:13:43 +0200

    fix env

--- src/.jest/setupEnvVars.ts
+++ src/.jest/setupEnvVars.ts
@@ -7,16 +7,6 @@ process.env = {
   S3_REGION: 's3-region',
   S3_BUCKET_NAME_RAW: 'fake-raw-bucket-name',
   S3_BUCKET_NAME_PDF: 'fake-pdf-bucket-name',
-  S3_BUCKET_NAME_PDF2TEXT_SUCCESS: 'fake-pdf2text-success-bucket-name',
-  S3_BUCKET_NAME_PDF2TEXT_FAILED: 'fake-pdf2text-failed-bucket-name',
   S3_BUCKET_NAME_DELETION: 'fake-deletion-bucket-nam',
-  S3_BUCKET_NAME_DELETION_PROCESSED: 'fake-deletion-processed-bucket-nam',
-  S3_BUCKET_NAME_NORMALIZED: 'fake-normalized-bucket-name',
-  DBSDER_API_URL: 'http://localhost:3000',
-  DBSDER_API_KEY: '24277eca-290c-40c3-9bf6-a370880d800b',
-  DBSDER_OTHER_API_KEY: '24277eca-290c-40c3-9bf6-a370880d800b',
-  DBSDER_POWERFUL_API_KEY: '24277eca-290c-40c3-9bf6-a370880d800b',
-  NLP_PSEUDONYMISATION_API_URL: 'http://localhost:8081',
   USE_AUTH: 'basic',
-  ZONING_API_URL: 'http://localhost:8090'
 }

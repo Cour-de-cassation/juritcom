33676d519ac5710f49fefcb26fd2d86723137a5a
Antoine Jeanneney <antoine.jeanneney@justice.fr>
2026-05-11 17:19:37 +0200

    fix

--- src/api/usecase/saveDecision.usecase.ts
+++ src/api/usecase/saveDecision.usecase.ts
@@ -2,7 +2,6 @@ import { MetadonneeDto } from '../../shared/infrastructure/dto/metadonnee.dto'
 import { v4 as uuidv4 } from 'uuid'
 import { DecisionS3Repository } from 'src/shared/infrastructure/repositories/decisionS3.repository'
 import { DecisionMongoRepository } from 'src/shared/infrastructure/repositories/decisionMongo.repository'
-import { ObjectId } from 'mongodb'
 
 export class SaveDecisionUsecase {
   private readonly decisionS3Repository: DecisionS3Repository = new DecisionS3Repository()
